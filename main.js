f25d5cd64dd111e6344d875c66d2b9b6046b45e2
luc1236060 <lau2000g@gmail.com>
2025-06-15 18:55:05 -0400

    cleaned up + description

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
 const images = [
-    'assets/img1.png',
-    'assets/img2.png',
-    'assets/img3.png',
-    'assets/img4.png',
-    'assets/img5.png'
+    {src: 'assets/img1.png', description: 'This is the starting page of the music app, features a play button, previous and next, search, volume controls, and an all songs view.' },
+    {src: 'assets/img2.png', description: 'The search function filters based on characters in song titles.'},
+    {src: 'assets/img3.png', description: 'Having no letters showcases the top 3 songs from the list'},
+    {src: 'assets/img4.png', description: 'All songs view showcases all available songs, the user can select a song from this list'},
+    {src: 'assets/img5.png', description: 'Each song has its own cover image and title.'},
 ]
 
 let index = 0;
 const viewer = document.getElementById('viewer');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
+const description = document.getElementById('description');
+
+function updateDisplay() {
+    viewer.src = images[index].src;
+    description.textContent = images[index].description;
+}
 
 prev.onclick = () => {
     index = (index - 1 + images.length) % images.length;
-    viewer.src = images[index];
+    updateDisplay();
 }
 
 next.onclick = () => {
     index = (index + 1) % images.length;
-    viewer.src = images[index];
+    updateDisplay();
 }
+
+updateDisplay();
