1ae765f64ef160ba58fef6793b229787e67cebfc
luc1236060 <lau2000g@gmail.com>
2025-06-20 14:56:43 -0400

    Updated images

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const images = [
     {src: 'assets/img1.png', description: 'This is the starting page of the music app, features a play button, previous and next, search, volume controls, and an all songs view.' },
-    {src: 'assets/img2.png', description: 'The search function filters based on characters in song titles.'},
-    {src: 'assets/img3.png', description: 'Having no letters showcases the top 3 songs from the list'},
-    {src: 'assets/img4.png', description: 'All songs view showcases all available songs, the user can select a song from this list'},
-    {src: 'assets/img5.png', description: 'Each song has its own cover image and title.'},
+    {src: 'assets/img2.png', description: 'Each song has its own cover image and title.'},
+    {src: 'assets/img3.png', description: 'The search function filters based on characters in song titles.'},
+    {src: 'assets/img4.png', description: 'Having no letters showcases the top 3 songs from the list'},
+    {src: 'assets/img5.png', description: 'All songs view showcases all available songs, the user can select a song from this list'},
+    {src: 'assets/img6.png', description: 'This view also has a Delete toggle.'},
+    {src: 'assets/img7.png', description: 'With the toggle on, clicking a song from the list will prompt the deletion of the song.'},
+    {src: 'assets/img8.png', description: 'There is also a New Song button, clicking it will prompt the user to upload an audio file, cover image, and song title, to add the song.'},
+    {src: 'assets/img9.png', description: 'At the top left of the app, a credits button can be clicked to show links to my github and portfolio website.'},
+
 ]
 
 let index = 0;
